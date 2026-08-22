c7784e686c053dd1c8331c0bc4b300e086bf9c1b
Thomalpas <92929876+Thomalpas@users.noreply.github.com>
2026-08-22 09:17:48 +0100

    fix conjoining local vs global

--- scripts/conjoining_stoch.jl
+++ scripts/conjoining_stoch.jl
@@ -1,17 +1,28 @@
 
 using Arrow, DataFrames
 
-hill = 2
+function combine_data()
+    hill = 2
+    dat = DataFrame()
 
-dat = DataFrame()
+    for start in 1:1000:89001
+        if start == 89001
+            temp = DataFrame(
+                Arrow.Table("./out/hill_$(hill)_stochastic_burnin_$(start)_89568.arrow")
+            )
+        else
+            temp = DataFrame(
+                Arrow.Table("./out/hill_$(hill)_stochastic_burnin_$(start)_$(start+999).arrow")
+            )
+        end
 
-for start in [1:1000:89001;]
-    if start == 89001
-        temp = DataFrame(Arrow.Table("./out/hill_$(hill)_stochastic_burnin_$(start)_89568.arrow"))
-    else
-        temp = DataFrame(Arrow.Table("./out/hill_$(hill)_stochastic_burnin_$(start)_$(start+999).arrow"))
+        dat = vcat(dat, temp)
     end
-    dat = vcat(dat, temp)
+
+    Arrow.write(
+        "./out/hill_$(hill)_stochastic_burnin_combined.arrow",
+        dat
+    )
 end
 
-Arrow.write("./out/hill_$(hill)_stochastic_burnin_combined.arrow", dat)
+combine_data()
